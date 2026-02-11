94b5891e72e852539cf0831785c129dcfbb06dfd
Patrick Patenaude <ppatenaude@hotmail.fr>
2026-02-10 20:09:48 -0500

    chore: remove obsolete debug scripts (check-version, test-rent)

--- scripts/check-version.ts
+++
@@ -1,8 +0,0 @@
-import { ethers } from "hardhat";
-
-async function main() {
-  const v2 = await ethers.getContractAt("CarRentalV2", "0x2BCA7b901cEc9049EBCF2695Be33735BeDc7f748");
-  console.log("getVersion():", await v2.getVersion());
-}
-
-main().catch(console.error);
